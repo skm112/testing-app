47f66ed23e7eb3aa14af9bc8d1d87eade414d866
skm112 <38988114+skm112@users.noreply.github.com>
2021-12-24 21:53:58 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { FC } from "react";
+import { View, Text } from "react-native";
+
+const App: FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Testing App</Text>
+    </View>
+  )
+}
+
+
+export default App
